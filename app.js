483ae6035f300494c54fcc80ce7bb6f4697293ac
LuoZhijie-tom <e0492460@u.nus.edu>
2021-06-27 16:17:01 +0800

    deploying attempt 2

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-
-app.listen(3000, () => {
-    console.log("port 3000 listening!")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
