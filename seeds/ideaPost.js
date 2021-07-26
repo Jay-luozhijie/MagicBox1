e97b94082580ca12ab7563588a6a7a430aa0d2e3
wwk227 <e0492479@u.nus.edu>
2021-07-26 11:02:08 +0800

    email verification on sign-up

--- seeds/ideaPost.js
+++ seeds/ideaPost.js
@@ -17,9 +17,10 @@ db.once("open", () => {
 
 const seedDB = async () => {
     await IdeaModel.deleteMany({});
-    for (let i = 1; i <= 10; i++) {
+    for (let i = 1; i <= 50; i++) {
         const idea1 = new IdeaModel({
-            author: `60d746baca590351d42cfcea`,
+            // author: `60d746baca590351d42cfcea`,
+            author: `60f6885430d1ae2e00f3c222`,
             title: `NUS Map`,
             description: `I’m a freshman in NUS. I’m not familiar with the location of NUS venues and thus often lost my way. I’m not from SOC and don’t know how to build an app, but it would be nice if there is an app that contains the NUS map and can direct me to the correct location of lecture theatre where I attend lectures. Also, since looking at a cell phone while walking is dangerous, this app should provide audio guidance while I’m walking.
             It must be great if the app can work like Google Map!`,
@@ -29,7 +30,8 @@ const seedDB = async () => {
         })
         await idea1.save();
         const idea2 = new IdeaModel({
-            author: `60d4021721179d2678f85a3c`,
+            // author: `60d4021721179d2678f85a3c`,
+            author: `60dfd69ed12bf82550deea05`,
             title: `Umbrella problem`,
             description: `Our normal umbrellas only keep us safe during light rain. When it rains very hard, or when the wind tilts it down, we get wet even with our umbrellas.
             Who can design an umbrella that won't get me wet in rainy days?`,
